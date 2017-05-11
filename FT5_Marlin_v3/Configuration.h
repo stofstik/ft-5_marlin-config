71e31bb1c2c2f4b393926388ad28cbe9a7a8f874
stofstik <verstegen.daan@gmail.com>
2017-05-11 23:43:14 +0200

    pid bed temp 60

--- FT5_Marlin_v3/Configuration.h
+++ FT5_Marlin_v3/Configuration.h
@@ -293,9 +293,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  #define  DEFAULT_bedKp 672.06
+  #define  DEFAULT_bedKi 54.69
+  #define  DEFAULT_bedKd 2064.56
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
