dc6e3d377a01cd6fc614435ee776cb2a5e04a5de
stofstik <verstegen.daan@gmail.com>
2017-05-11 22:47:50 +0200

    bed pid tune

--- Configuration.h
+++ Configuration.h
@@ -293,9 +293,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  #define  DEFAULT_Kp 25.73
+  #define  DEFAULT_Ki 2.05
+  #define  DEFAULT_Kd 80.95
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
