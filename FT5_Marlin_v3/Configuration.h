a11538e2a3b89b984aed1b56008487949ad36fda
stofstik <verstegen.daan@gmail.com>
2017-05-13 11:53:28 +0200

    tune max x

--- FT5_Marlin_v3/Configuration.h
+++ FT5_Marlin_v3/Configuration.h
@@ -493,7 +493,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 #define X_MIN_POS -5
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 300
+#define X_MAX_POS 290
 #define Y_MAX_POS 300
 #define Z_MAX_POS 380
 
