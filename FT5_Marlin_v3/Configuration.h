c3ed4d30d25a5409256f5877523a2d9b715fd507
stofstik <verstegen.daan@gmail.com>
2017-06-26 22:30:22 +0200

    installed 280W 12V heated bed with separate mosfet

--- FT5_Marlin_v3/Configuration.h
+++ FT5_Marlin_v3/Configuration.h
@@ -286,9 +286,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 672.06
-  #define  DEFAULT_bedKi 54.69
-  #define  DEFAULT_bedKd 2064.56
+  #define  DEFAULT_bedKp 391.52
+  #define  DEFAULT_bedKi 56.10
+  #define  DEFAULT_bedKd 683.16
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
